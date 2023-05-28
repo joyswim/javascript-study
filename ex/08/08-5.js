98769175abc3d227ea54dee7cb898b43e5791a9f
suyoung <joyswim@naver.com>
2023-05-28 14:22:07 +0900

    delete all files

--- ex/08/08-5.js
+++
@@ -1,6 +0,0 @@
-//08-5
-var num = 3;
-
-var result = num % 2 ? '홀수' : '짝수';
-
-console.log(result);
--- ex/08/08-6.js
+++
@@ -1,6 +0,0 @@
-//08-6
-var num = 2;
-
-var kind = num ? (num > 0 ? '양수' : '음수') : '영';
-
-console.log(kind);
